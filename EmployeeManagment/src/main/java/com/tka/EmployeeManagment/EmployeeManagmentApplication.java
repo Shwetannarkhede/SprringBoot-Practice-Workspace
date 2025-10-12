34292d5df0006c76e402f984ec83b1e6c4a7572a
Shweta <shwetanarkhede52@gmail.com>
2025-10-12 21:23:23 +0530

    Initial commit

---
+++ EmployeeManagment/src/main/java/com/tka/EmployeeManagment/EmployeeManagmentApplication.java
@@ -0,0 +1,14 @@
+package com.tka.EmployeeManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagmentApplication.class, args);
+		System.out.println("Welcome to SpringBoot world");
+	}
+
+}
---
+++ StudentManagment/src/test/java/com/tka/StudentManagment/StudentManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tka.StudentManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
